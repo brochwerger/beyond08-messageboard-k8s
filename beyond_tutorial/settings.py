6a95e2b5d33835662b4b49ac803f28267c94932c
Benny Rochwerger <brochwer@redhat.com>
2023-01-01 19:53:36 +0200

    DEMO 3: Raw K8S
        Added YAML files for deploying on raw kubernetes
    
        - Use secrets for database credentials
        - Use PVC and PV for database storage
        - Modified django settings to read db credentials from env
    
    Signed-off-by: Benny Rochwerger <brochwer@redhat.com>

--- beyond_tutorial/settings.py
+++ beyond_tutorial/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,9 +78,9 @@ WSGI_APPLICATION = 'beyond_tutorial.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'messages',
-        'USER': 'beyond',
-        'PASSWORD': 'beyond',
+        'NAME': os.environ['POSTGRES_DB'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
         'HOST': 'dbserver',
         'PORT': '5432',
     }

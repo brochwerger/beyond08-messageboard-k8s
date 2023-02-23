bc0a1df425df5f0556414936b70a71190ca35f48
Benny Rochwerger <brochwer@redhat.com>
2023-02-23 15:33:26 +0200

    Enabled access from outside container

--- beyond_tutorial/settings.py
+++ beyond_tutorial/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tqks5a_6ze#$_o!=7um!)c$+cpxz-5h$q_re9)7xu!s9(f39&-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
